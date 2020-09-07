7362f76bf33e07ccbecbe2cb9c82a53e6c9c73be
kennedn <kennedn@msn.com>
2020-09-08 00:07:45 +0100

    Tile generation now controlled by JSON objects
    Startings of a complete refactor. Hard coded values have been stripped out
    and replaced with a JSON loading system that allows navigation via localStorage.

--- library/tiles.js
+++ library/tiles.js
@@ -4,19 +4,23 @@ if (event.persisted) {
 }
 };
 
-function resizeOnCanvas(time) {
+function isLinkLocal(link) {
+    return (link.host !== window.location.host);
+}
+
+function resizeOnCanvas(time, tileSqrt) {
         let object = $(".tile-container-big > .tile-big > .back > .wrapperLeft, .wrapperTop").children("iframe").contents().find("canvas");
         if(object.length > 0) {
-            resizeTile();
+            resizeTile(tileSqrt);
             return;
         }
         else {
             setTimeout(function() {
-                resizeOnCanvas(time);
+                resizeOnCanvas(time, tileSqrt);
             }, time);
         }
 }
-function resizeTile() {
+function resizeTile(tileSqrt) {
   
   // Get Window dimensions
   let WIDTH = $(window).outerWidth();
@@ -24,8 +28,8 @@ function resizeTile() {
   let docSize = Math.min(WIDTH, HEIGHT);
 
   // Calculate maximum width / height each tile could have to fit in current window (minus a small pad)
-  let tileWidth= Math.floor((WIDTH * (1/columns)) - (WIDTH * 0.002));
-  let tileHeight= Math.floor((HEIGHT * 1/rows) - (HEIGHT * 0.009));
+  let tileWidth= Math.floor((WIDTH * (1/tileSqrt)) - (WIDTH * 0.002));
+  let tileHeight= Math.floor((HEIGHT * 1/tileSqrt) - (HEIGHT * 0.009));
   // Get lowest of the two and use this as the tile width & height going forward
   let tileSize=Math.min(tileWidth,tileHeight);
 
@@ -39,29 +43,29 @@ function resizeTile() {
   $(".tile-container, .tile > .front, .tile > .back").css(
     {'width': tileSize, 'height': tileSize});
   $(".tile-container-big, .tile-big > .front, .tile-big > .back").css(
-    {'width': tileSize*columns, 'height': tileSize*rows});
+    {'width': tileSize*tileSqrt, 'height': tileSize*tileSqrt});
 
   // Move the center div to the center of the screen using our derived tile sizes
   $('.center').css({
      'position' : 'absolute',
      'left' : '50%',
      'top' : '50%',
-     'margin-left' :-(tileSize*rows)/2,
-     'margin-top' : -(tileSize*columns)/2,
-     'width': tileSize * rows,
-     'height': tileSize * columns
+     'margin-left' :-(tileSize*tileSqrt)/2,
+     'margin-top' : -(tileSize*tileSqrt)/2,
+     'width': tileSize * tileSqrt,
+     'height': tileSize * tileSqrt
   });
 
 
   $(".tile-container-big > .tile-big > .back > .wrapperLeft, .wrapperTop").each(function() {
-      var $wrap = $(this);
-      var wrapWidth = $wrap.width(); // width of the wrapper
-      var wrapHeight = $wrap.height();
-      var childWidth = $wrap.children("iframe").contents().find("canvas").width();  // width of child iframe
-      var childHeight = $wrap.children("iframe").contents().find("canvas").height(); // child height
-      var wScale = wrapWidth / childWidth;
-      var hScale = wrapHeight / childHeight;
-      var scale = Math.min(wScale,hScale);  // get the lowest ratio
+      let $wrap = $(this);
+      let wrapWidth = $wrap.width(); // width of the wrapper
+      let wrapHeight = $wrap.height();
+      let childWidth = $wrap.children("iframe").contents().find("canvas").width();  // width of child iframe
+      let childHeight = $wrap.children("iframe").contents().find("canvas").height(); // child height
+      let wScale = wrapWidth / childWidth;
+      let hScale = wrapHeight / childHeight;
+      let scale = Math.min(wScale,hScale);  // get the lowest ratio
       $wrap.children("iframe").css({"transform": "scale("+scale+")", "transform-origin": "left top" });  // set scale
       $wrap.children("iframe").css({'height': wrapHeight*1/(Math.floor(scale * 10)/10)});
       $wrap.children("iframe").css({'width': wrapWidth*1/(Math.floor(scale * 10)/10)});
@@ -74,43 +78,33 @@ function resizeTile() {
   $(".tile .back h1").css({'font-size' :32 * fontScaler});
   $("h1").css({'font-size' :32 * fontScaler});
 
-  var canvas = $(".tile-container-big > .tile-big > .back > .wrapperLeft, .wrapperTop").children("iframe").contents().find("canvas");
+  let canvas = $(".tile-container-big > .tile-big > .back > .wrapperLeft, .wrapperTop").children("iframe").contents().find("canvas");
   if (canvas.length === 0)
-    resizeOnCanvas(120);
+    resizeOnCanvas(120, tileSqrt);
 }
 
 
 
 $(document).ready(function() {
-  var colors, hrefs, colFade, j, lastFlipped, icons, tile_y_origin;
-  colors = ["#1c817e", "#dd2164", "#6e04a9",
-            "#dd2164", "#6e04a9", "#1c817e",
-            "#6e04a9", "#1c817e", "#dd2164"];
-  //hrefs = [["https://www.android.com/", "https://www.raspberrypi.org/", "https://www.microsoft.com"];
-  icons = [["/images/bio_icon.png", "/images/stuff_icon.png", "/images/contact_icon.png"],
-           ["/images/stuff_icon.png", "/images/contact_icon.png", "/images/bio_icon.png"],
-           ["/images/contact_icon.png", "/images/bio_icon.png", "/images/stuff_icon.png"]];
-  titles = [["Bio", "Stuff I've made", "Contact"],
-            ["Stuff I've made", "Contact", "Bio"],
-            ["Contact", "Bio", "Stuff I've made"]];
-  paras = [["A summary of who I am, where I've been and what i've done.", "Be it games, some hacky bash scripts or some random project.", "How you can get in touch."],
-           ["Be it games, some hacky bash scripts or some random project.", "How you can get in touch.", "A summary of who I am, where I've been and what i've done."],
-           ["How you can get in touch.", "A summary of who I am, where I've been and what i've done.", "Be it games, some hacky bash scripts or some random project."]];
-  colFade = 400;
-  columns = 3;
-  rows=3;
-  removeTiles = false;
+  let lastFlipped;
+  let jsonFile = false;
+  jsonFile = localStorage.getItem('jsonFile');
+  localStorage.removeItem('jsonFile');
+  if(!jsonFile)
+    jsonFile = "/json/main.json";
 
-  
-  storedValue = 'None';
-  storedValue = localStorage.getItem('caller', storedValue);
-  if(storedValue) {
-    $("body").css("background-color", storedValue);
-    $("#background").css("background-color", storedValue);
+  let tileSet = JSON.parse($.getJSON({'url': jsonFile, 'async': false}).responseText).tiles;
+  let colFade = 400;
+  let tileSqrt = Math.max(Math.ceil(Math.sqrt(tileSet.length)), 2);
+  let lastColor = false;
+  lastColor = localStorage.getItem('lastColor');
+  if(lastColor) {
+    $("body").css("background-color", lastColor);
+    $("#background").css("background-color", lastColor);
   }
   else {
-    $("body").css("background-color", colors[1]);
-    $("#background").css("background-color", colors[1]);
+    $("body").css("background-color", tileSet[1].color);
+    $("#background").css("background-color", tileSet[1].color);
   }
 
   
@@ -119,39 +113,50 @@ $(document).ready(function() {
                   <div id=0 class="tile-big">
                     <div class="front">`;
 
-  for (y=0;y<rows;y++) {
-	  for (x=0;x<columns;x++) {
-	    tileHTML+=`<div class="tile-container">
-	      <div id="${x + 1 + columns * y}" class="tile">
-	
-	        <div class="front">
-	          <h1> ${titles[y][x]} </h1>
-	          <img onmousedown="return false" src="${icons[y][x]}"/>
-	        </div>
-	
-	        <div class="back">
-	          <img onmousedown="return false" src="/images/tile_back.png"/>
-	          <h1> ${titles[y][x]} </h1>
-	
-	        <div class="p-bg"> 
-	            <p> ${paras[y][x]} </p>
-	        </div> 
-	
-	        </div>
-	        
-	      </div>
-	    </div>`;
-	  }
-    tileHTML += `</br>`;
-  }
+  tileSet.forEach((tile, i) => {
+    tileHTML+=`<div class="tile-container">
+      <div id="${i+1}" class="tile" style="${tile.active ? '' : 'display:none; cursor:default;'}">
+
+        <div class="front">
+          <h1> ${tile.title} </h1>
+          <img onmousedown="return false" src="${tile.icon}"/>
+        </div>
+
+        <div class="back">
+          <img onmousedown="return false" src="/images/tile_back.png"/>
+          <h1> ${tile.title} </h1>
+
+        <div class="p-bg"> 
+            <p> ${tile.text} </p>
+        </div> 
+
+        </div>
+        
+      </div>
+    </div>`;
+    //<div class="p-bg"><p id="big-p"> ${paras[0][0]} </p></div></div></div></div><div class='wrapperTop'><iframe seamless scrolling='no' frameBorder='0' class='gameFrame' src='/pong/index.html'></iframe></div>
+    // Determine break points in the tile list so that we get as close to 
+    if(i != 0 && i + 1 % tileSqrt == 0)
+      tileHTML += `</br>`;
+  });
 
   
   tileHTML += `</div>
                <div class="back">
-                  <img onmousedown="return false" src="/images/tile_back.png"/>`;
+                  <img onmousedown="return false" src="/images/tile_back.png"/>
+                  <div class="p-bg">
+                    <p id="big-p"> This is a big frickin tile my dude. </p>
+                  </div>
+                  <div class='wrapperTop'>
+                    <iframe seamless scrolling='no' frameBorder='0' class='gameFrame' src='/gravitation/index.html'></iframe>
+                  </div>
+                </div>
+                </div>
+                </div>
+                `;
   $("body").append(tileHTML);
 
-  resizeTile();
+  resizeTile(tileSqrt);
   window.onresize = resizeTile;
 
 
@@ -159,90 +164,65 @@ $(document).ready(function() {
   // Setting this within the stylesheet to 50% does not seem to work correctly, this is a workaround
   $(".tile").css("transform-origin", "50% " + $(".tile-container .front").css("height").match(/\d+/) / 2 + "px");
   // For each tile        
-  for (j=1;j<=columns * rows;j++) {
-    //watch for a click on the div with id j
-    $("#"+j).click(function() {
+  for(let i=1;i <= tileSet.length; i++){
+    //Rotate back from 90 deg end-flipped state
+    $("#"+i+ " .front, .back").hide().fadeIn(400);
 
+    //watch for a click on the div with id i
+    $("#"+i).click(function() {
       // Set our id again
-      j = Number(this.id);
+      i = Number(this.id);
       // If we did not click the same tile twice
-      if( j != lastFlipped ) {  
+      if( i != lastFlipped ) {  
         // Rotate clicked tile
         $(this).toggleClass('flipped');
         // Animate the paragraph backgrounds height
         $(this).find(".p-bg").animate({height: '58%'},600,"swing", function() {
-            $("#0 > .back > .wrapperLeft").remove();
-            $("#0 > .back > .wrapperTop").remove();
-            if(j === columns * rows) {
-              $("#0 > .back").append(`</div></div></div><div class='wrapperLeft'><iframe seamless scrolling='no' frameBorder='0' class='gameFrame' src='/tetris/index.html'></iframe></div>`);
-              resizeOnCanvas(100);
-            }
-            else if(j === columns * rows - 1) {
-              $("#0 > .back").append(`<div class="p-bg"><p id="big-p"> ${paras[0][0]} </p></div></div></div></div><div class='wrapperTop'><iframe seamless scrolling='no' frameBorder='0' class='gameFrame' src='/pong/index.html'></iframe></div>`);
-              resizeOnCanvas(100);
-            }
-            else if(j === columns * rows - 2) {
-              $("#0 > .back").append(`<div class="p-bg"><p id="big-p"> ${paras[0][0]} </p></div></div></div></div><div class='wrapperTop'><iframe seamless scrolling='no' frameBorder='0' class='gameFrame' src='/gravitation/index.html'></iframe></div>`);
-              resizeOnCanvas(100);
-            }
+          // Check if we are a big tile, if so replace text and iframe src
+            // $("#0 > .back > .wrapperLeft").remove();
+            // $("#0 > .back > .wrapperTop").remove();
         });    
         
   
         // Set body bg color, show background div then fade it, setting the correct color once it is opaque 
         // In short this does a nice fade effect on the background color
-        $("body").css("background-color", colors[j - 1]);
+        $("body").css("background-color", tileSet[i - 1].color);
         $("#background").show().fadeOut(colFade, "linear", function() {
-            $(this).css("background-color", colors[j - 1]); 
+            $(this).css("background-color", tileSet[i - 1].color); 
           });
   
-        // If we clicked another tile from the last one, reset the previous tiles parameters.
-        
           // reflip the last tile
           $("#"+lastFlipped).toggleClass('flipped');
           //reset the paragraph background to 1% height
           $("#"+lastFlipped).find(".p-bg").animate({height: '1%'},400);
       }
       // flip tile back around
-      else if(j === columns * rows) {
+      else if(i === tileSet.length -2) {
         $("#0").toggleClass('flipped');
-        $("#0").find(".p-bg").animate({height: '40%'},800,"swing");
-      }
-      else if(j === columns * rows - 1) {
-        $("#0").toggleClass('flipped');
-        $("#0").find(".p-bg").animate({height: '40%'},800,"swing");
-      }
-      else if(j === columns * rows - 2) {
-        $("#0").toggleClass('flipped');
-        $("#0").find(".p-bg").animate({height: '40%'},800,"swing");
+        $("#0").find(".p-bg").animate({height: '30%'},800,"swing");
+        resizeTile(tileSqrt);
       }
       // Ending animations.
       else {
-        // For each tile that isnt the one we just clicked
-        for(var k=1;k<=columns * rows;k++) {
-          // swap out the img src to the tile that we just clicked
-          //$("#"+k).find(".front").find("img").attr("src", icons[lastFlipped-1]);
-          // If we didnt click this tile last
-          if (k != lastFlipped) {
-            // flip the tile 90deg (eg. edge facing us)
-            $("#"+k).toggleClass('end-flipped');
-          }
-          else {
-            $("#"+k).toggleClass('flipped');
-            (function(k){
-              $("#"+k +" .front").delay(400).fadeOut(500, function() {
-            
-              //location.reload();
-              localStorage.setItem('caller', colors[k-1]);
-              //window.location = "/index.html"
-              location.reload();
-              });
-            })(k);
+        // For each tile that isnt the one we iust clicked
+        for(let k=1;k<=tileSet.length;k++) {
+            $("#"+k+ " .front, .back").fadeOut(400, () => {
+                k = this.id;
+                if( k == lastFlipped){
+                  localStorage.setItem('lastColor', tileSet[k-1].color);
 
-            // Fade the tile then navigate to url afterwards
-          }
+                  if(tileSet[k-1].url.split('.').pop() === 'json') {
+                    localStorage.setItem('jsonFile', tileSet[k-1].url);
+                    location.reload();
+                    //location.href = location.href;
+                  }
+                }
+                else
+                  window.location.href = tileSet[k-1].url;
+            });
         }
       }       
       lastFlipped = Number(this.id);
     });
-  };
+  }
 });
