0b2d641e95d545d64dc8e474f3598028850723c3
kennedn <kennedn@msn.com>
2021-03-28 21:55:34 +0100

    Added additional zoom disable and typo fixing

--- library/nozoom.js
+++ library/nozoom.js
@@ -24,4 +24,8 @@ if (!window.document.documentMode) {
 	    if(event.touches.length > 1)
 	        event.preventDefault();
 	}, {passive: false});
+	document.addEventListener('touchmove', (event) => {
+	if (event.scale !== 1)
+	        event.preventDefault();
+	}, {passive: false});
 }
