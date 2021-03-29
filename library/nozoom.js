8584a5498ba688c2de4ba0cd312c10845fe65c3e
kennedn <kennedn@msn.com>
2021-03-29 20:19:52 +0100

    Fixed safari double click issues on iOS 14

--- library/nozoom.js
+++ library/nozoom.js
@@ -19,18 +19,19 @@ if (!window.document.documentMode) {
 	       event.preventDefault();
 	}, {passive: false}); 
 
-	
-	document.addEventListener('touchmove', (event) => {
-	if (event.scale !== 1)
-	        event.preventDefault();
-	}, {passive: false});
+  function preventZoom(e) {
+      var t2 = e.timeStamp;
+      var t1 = e.target.dataset.lastTouch || t2;
+      var dt = t2 - t1;
+      var fingers = e.touches.length;
+      e.target.dataset.lastTouch = t2;
 
-	var doubleTouchStartTimestamp = 0;
-	document.addEventListener("touchstart", function(event){
-    	var now = +(new Date());
-    	if (doubleTouchStartTimestamp + 500 > now || event.touches.length > 1){
-        	event.preventDefault();
-    	}
-    	doubleTouchStartTimestamp = now;
-	});
+      if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+
+      e.preventDefault();
+      e.target.click();
+  }
+
+	document.addEventListener('touchend', preventZoom);
+
 }
